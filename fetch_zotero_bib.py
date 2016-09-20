4ec721b9da3043f93579148b339b94eb04f16ba3
Danny Chan <danwchan@gmail.com>
2016-09-20 15:00:33 -0500

    made a script to fetch a zotero collection as bibtex

--- fetch_zotero_bib.py
+++ fetch_zotero_bib.py
@@ -1,4 +1,18 @@
 from pyzotero import zotero
-from secrets import
 
-zot = zotero.Zotero(userID, 'user', key)
+#this is your Zotero login info
+import secrets
+
+library = zotero.Zotero(userID, 'user', key)
+
+for c in library.collections():
+  if c['data']['name'] == "my_publications":
+    fetchID = c['key']
+
+fetchbibtex = zot.collection_items(fetchID, include = 'bibtex')
+
+with open('my_publications.bib', 'w') as file:
+  for items in fetchbibtex:
+    file.write(items['bibtex'].encode('utf-9'))
+
+file.close()
